d3d7391a0f71e1f94af7b95ac2aa07fc18f22e1e
Eric Lin <c84lin@uwaterloo.ca>
2020-04-28 23:25:34 -0400

    added CORS

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import os
 import cherrypy
+import cherrypy_cors
 
 class Server():
     @cherrypy.expose
@@ -7,10 +8,14 @@ class Server():
         return "Server is up and running"
 
 if __name__ == "__main__":
+    cherrypy_cors.install()
     server = Server()
     cherrypy.config.update({"server.socket_host": "0.0.0.0"})
     cherrypy.config.update(
-        {"server.socket_port": int(os.environ.get("PORT", "8080")),}
+        {
+            "server.socket_port": int(os.environ.get("PORT", "8080")),
+            'cors.expose.on': True,
+        }
     )
     print("Starting Bad Chess Server...")
     cherrypy.quickstart(server)
