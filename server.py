fcda8df3a311b16a1e0c568926168db6e66846bb
Eric Lin <c84lin@uwaterloo.ca>
2020-04-28 23:50:33 -0400

    moved back to update config

--- server.py
+++ server.py
@@ -10,12 +10,10 @@ class Server():
 if __name__ == "__main__":
     cherrypy_cors.install()
     server = Server()
-    config = {
-        "/": {
-            "server.socket_host": "0.0.0.0",
-            "server.socket_port": int(os.environ.get("PORT", "8080")),
-            "cors.expose.on": True,
-        }
-    }
+    cherrypy.config.update({
+        "server.socket_host": "0.0.0.0",
+        "server.socket_port": int(os.environ.get("PORT", "8080")),
+        "cors.expose.on": True,
+    })
     print("Starting Bad Chess Server...")
-    cherrypy.quickstart(server, config=config)
+    cherrypy.quickstart(server)
