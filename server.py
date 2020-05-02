cad8c12a8e33b16000a92756f9dbfeaa31e9f2c6
Eric Lin <c84lin@uwaterloo.ca>
2020-05-02 15:43:50 -0400

    created simple single player endpoint

--- server.py
+++ server.py
@@ -1,11 +1,50 @@
 import os
 import cherrypy
 import cherrypy_cors
+import time
 
 class Server():
+    def __init__(self):
+        self.games = {}
+
     @cherrypy.expose
     def index(self):
         return { "data": "Server is up and running" }
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def create_single_player_game(self):
+        # create unique game ID (maybe based off time?)
+        gid = int(time.time())
+
+        # send back game state data with game ID
+        game_state = {
+            "game_id": gid,
+            "board": [
+                [["R", False], ["H", False], ["B", False], ["K", False], ["Q", False], ["B", False], ["H", False], ["R", False]],
+                [["P", False], ["P", False], ["P", False], ["P", False], ["P", False], ["P", False], ["P", False], ["P", False]],
+                [["N", True], ["N", True], ["N", True], ["N", True], ["N", True], ["N", True], ["N", True], ["N", True]],
+                [["N", True], ["N", True], ["N", True], ["N", True], ["N", True], ["N", True], ["N", True], ["N", True]],
+                [["N", True], ["N", True], ["N", True], ["N", True], ["N", True], ["N", True], ["N", True], ["N", True]],
+                [["N", True], ["N", True], ["N", True], ["N", True], ["N", True], ["N", True], ["N", True], ["N", True]],
+                [["P", True], ["P", True], ["P", True], ["P", True], ["P", True], ["P", True], ["P", True], ["P", True]],
+                [["R", True], ["H", True], ["B", True], ["K", True], ["Q", True], ["B", True], ["H", True], ["R", True]]
+            ]
+        }
+
+        return game_state
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def game_exists(self):
+        data = cherrypy.request.json
+        if data["game_id"] in self.games:
+            data["game_exists"] = "True"
+        else:
+            data["game_exists"] = "False"
+        return data
+
 
 if __name__ == "__main__":
     cherrypy_cors.install()
