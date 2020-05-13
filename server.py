e5b03b90a9a306511e19161282f06c14bdb92e24
Eric Lin <c84lin@uwaterloo.ca>
2020-05-13 13:06:58 -0400

    log whenever a player leaves a game

--- server.py
+++ server.py
@@ -142,6 +142,7 @@ class Server():
             player_id = data["playerID"]
 
             # if no player is left, remove from memory
+            log(f"Disconnecting PID: {player_id} from GID: {gid}")
             self.game_by_game_id[gid].disconnect(player_id)
             if not self.game_by_game_id[gid].any_connected():
                 room_code = self.game_by_game_id[gid].room_code
